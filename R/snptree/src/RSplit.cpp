4a51b3aa787b66df8b30368f681770ad478aa511
Ian Wilson <wilson.ianj@gmail.com>
2017-03-09 00:02:59 +0000

    making more changes

--- R/snptree/src/RSplit.cpp
+++ R/snptree/src/RSplit.cpp
@@ -26,9 +26,9 @@ extern "C" {
     for (int i=0;i<*npos;i++) s.split(positions[i]);   // split at positions
     *len=s.nleaves();                                  // how many leaves do we have on the tree
     
-    std::vector<std::pair<int,int> > edges;
-    std::vector<std::vector<int> > labels;
-    
+    std::vector<std::pair<int,int> > edges;            // set up date structure for edges
+    std::vector<std::vector<int> > labels;             // and for labels  
+     
     s.apesplit(edges, labels);
     
     // get the lengths of the labels to allow us to pass this information back to R
@@ -42,7 +42,7 @@ extern "C" {
     
     int nedges=static_cast<int>(edges.size());
     if (nedges != 2*(*len-1))
-      throw std::range_error("problem in split_simple\n");
+      throw std::range_error("problem in C++ code split_simple\n");
     
     for (int i=0; i<nedges; i++) {
       edge[i] = edges[i].first;
@@ -61,30 +61,17 @@ extern "C" {
  /** Get a split that splits everything (if no information then the 
   * splits are at random                                               */
  void simplesplit( int *data, int *samplesize, int *nSNP, 
-                   int *edge,int *comblabels) 
+                   int *xxx, int *nleaves) 
  {  
    TNT::Array2D<int> d(*samplesize,*nSNP,data);
    splitter<int> s(d,d.dim1(),d.dim2());
    for (int i=0;i<*nSNP;i++) s.split(i);
    
-   while(s.fullsplit());
+   *nleaves=s.nleaves();  
+   int nedges = 2*(*nleaves-1);
    
-   std::vector<std::pair<int,int> > edges;
-   std::vector<std::vector<int> > labels;
-   
-   s.apesplit(edges,labels);
-   
-   
-   int nedges=static_cast<int>(edges.size());
-   if (nedges!=2*(*samplesize-1))
-     throw std::runtime_error("problem in C++ code simplesplit\n");
-   
-   for (size_t j=0;j<labels.size();j++) comblabels[j] = labels[j][0];
-   
-   for (int i=0;i<nedges;i++) {
-     edge[i]=edges[i].first;
-     edge[nedges+i]=edges[i].second;
-   }
+   s.edges_positions_counts(xxx, xxx+nedges, xxx+2*nedges, xxx+3*nedges);
+
  }
   
   
